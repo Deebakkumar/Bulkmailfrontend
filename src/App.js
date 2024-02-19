17f610fc4100367e9d5f86360806ed6d66f056ee
Deebakkumar <deebakkumar6682@gmail.com>
2024-02-19 12:12:26 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import "./App.css";
+import Compose from "./Components/Compose/Compose";
+import Emailverify from "./Components/Emailverify/Emailverify";
+import Graph from "./Components/Graph/Graph";
+import Home from "./Components/Home/Home";
+import Log from "./Components/Log/Log";
+import Login from "./Components/Login/Login";
+import NavComp from "./Components/Navbar/Navbar";
+import PasswordRes from "./Components/Password-Reset/PasswordRes";
+import ProtectedRoute from "./Components/ProtectedRoute";
+import ResetPassComp from "./Components/ResetPassComp/ResetPassComp";
+import Settings from "./Components/Settings/Settings";
+import Signup from "./Components/Signup/Signup";
 
 function App() {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (localStorage.getItem("x-Auth-token")) {
+      navigate("/")
+    }
+    
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavComp  />
+      <Routes>
+        <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+        <Route path="/compose"        element={<ProtectedRoute><Compose />    </ProtectedRoute>} />
+        <Route path="/graph"          element={<ProtectedRoute><Graph />      </ProtectedRoute>} />
+        <Route path="/log"            element={<ProtectedRoute><Log />        </ProtectedRoute>} />
+        <Route path="/settings"       element={<ProtectedRoute><Settings />   </ProtectedRoute>} />
+        <Route path="/login"          element={<Login />} />
+        <Route path="/password-reset" element={<PasswordRes />} />
+        <Route path="/signup"         element={<Signup />} />
+        <Route
+          path="/pas-reset-completion/:string"
+          element={<ResetPassComp />}
+        />
+        <Route path="/emailverify/:string" element={<Emailverify />} />
+      </Routes>
     </div>
   );
 }
